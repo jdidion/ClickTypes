40e65acebce0af55b97ddd49fd2702141576d741
John Didion <github@didion.net>
2018-12-03 06:17:46 -0500

    add test

--- tests/test_click_types.py
+++ tests/test_click_types.py
@@ -1,43 +1,20 @@
-from typing import Optional
-import clicktypes
+import sys
+import unittest
+import tests.simple
 
 
-@clicktypes.composite(
-    required=["a"]
-)
-class Foo:
-    """
-    A Foo.
-
-    Args:
-        a: blah
-        b: blorf
-    """
-    def __init__(self, a: int, b: str = "foo"):
-        self.a = a
-        self.b = b
-
-    def __str__(self):
-        return "{} {}".format(self.a, self.b)
-
-
-@clicktypes.command()
-def main(foo: Foo, *args, bar: int = 1, baz: Optional[float] = None, **kwargs):
-    """Process some metasyntactic variables.
-
-    Args:
-        foo: A Foo
-        *args: Extra args
-        bar: A bar
-        baz: A baz
-        **kwargs: Extra kwargs
-    """
-    print(foo)
-    print(args)
-    print(bar)
-    print(baz)
-    print(kwargs)
-
-
-if __name__ == "__main__":
-    main()
+class SimpleTest(unittest.TestCase):
+    def test_simple(self):
+        sys.argv = ["main", "1"]
+        try:
+            tests.simple.main()
+        except SystemExit:
+            pass
+        expected = dict(
+            foo=tests.simple.Foo(1),
+            args=(),
+            bar=1,
+            baz=None,
+            kwargs={}
+        )
+        assert tests.simple.RESULT == expected
